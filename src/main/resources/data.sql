c88cbacc84ff39e0766b9848c5484d96328701fc
PedroMDAndre <Molly/2020>
2021-11-01 18:05:14 +0000

    set initial database data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO cashier (name) VALUES
+  ('Pete'),
+  ('James'),
+  ('Ines');
+
+INSERT INTO user (name, surname, mobile_number, id_card_number) VALUES
+  ('Olga'  , 'Deas', '961123145', '753159'),
+  ('Vera'  , 'Kout', '961789143', '741852') ,
+  ('Viktor', 'King', '961456311', '985412');
